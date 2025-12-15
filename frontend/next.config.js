dfee81377a23487b9deea3480e009023318a360d
Majid Hadji <majidhadji@gmail.com>
2025-12-15 13:55:18 -0800

    triger the build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
                 destination: process.env.API_URL
                     ? `${process.env.API_URL}/:path*`
                     : 'http://127.0.0.1:8000/:path*', // Default to localhost for dev
-                // In Production (Cloud Run), ensure API_URL is set to your Backend Service URL.
+                // In Production (Cloud Run),ensure API_URL is set to your Backend Service URL.
             },
         ]
     },
